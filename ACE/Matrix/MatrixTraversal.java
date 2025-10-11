1796136560c3a1b97290dab113ae2c6961523473
Vishal <22k20.vishal@sjec.ac.in>
2025-10-11 14:38:04 +0530

    matrix traversal

---
+++ ACE/Matrix/MatrixTraversal.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class MatrixTraversal {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number of rows");
+        int m=sc.nextInt();
+        System.out.println("enter the number of columns");
+        int n=sc.nextInt();
+        int[][] matrix=new int[m][n];
+        System.out.println("enter the matrix elements");
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                matrix[i][j]=sc.nextInt();
+            }
+        }
+        System.out.println("matrix elements are");
+         for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+    
+}
