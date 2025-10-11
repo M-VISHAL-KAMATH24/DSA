00509d7e21210bc6c517f7d73a2ad61e30219130
Vishal <22k20.vishal@sjec.ac.in>
2025-10-11 15:02:42 +0530

    transpose of matrix

---
+++ ACE/Matrix/TransposeMatrix.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class TransposeMatrix {
+    public static void main(String args[]){
+        Scanner sc =new Scanner(System.in);
+        System.out.println("enter the size of the square matrix");
+        int n=sc.nextInt();
+        int[][] matrix=new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                matrix[i][j]=sc.nextInt();
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+        System.out.println("transpose of the matrix");
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
